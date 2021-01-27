6d712dd7869f90ed8914913242edb9b48ef018a7
Kirtirajsinh Solanki <kirtiraj2310@gmail.com>
2021-01-27 09:15:59 +0530

    Subproject and sub opportunity added

---
+++ src/projects/bms_casa/migrations/16_sub_project_master.ts
@@ -0,0 +1,62 @@
+import { QueryInterface, DataTypes } from "sequelize"
+module.exports = {
+    up: async (query: QueryInterface) => {
+        await query.createTable("sub_project_master", {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            subProjectName: {
+                type: DataTypes.STRING(45),
+                allowNull: false,
+            },
+            projectId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "project_master",
+                    key: "id",
+                },
+            },
+            constructionCompanyId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "construction_company_master",
+                    key: "id",
+                },
+            },
+            projectManagerId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "project_manager_master",
+                    key: "id",
+                },
+            },
+            salesPersonName: {
+                type: DataTypes.STRING(45),
+                allowNull: false,
+            },
+            note: {
+                type: DataTypes.STRING(200),
+                allowNull: true,
+            },
+            amount: {
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+            },
+            status: {
+                type: DataTypes.STRING(45),
+                allowNull: false,
+            },
+            createdOn: {
+                type: DataTypes.DATE,
+            },
+            modifiedOn: {
+                type: DataTypes.DATE,
+            },
+        })
+    },
+    down: async (query: QueryInterface) => {
+        await query.dropTable("sub_project_master")
+    },
+}
