c4ea5503ac341d767c6c1ec2c7728f8a6ae5d36c
Kirtirajsinh Solanki <kirtiraj2310@gmail.com>
2021-01-30 13:42:36 +0530

    quotation section updated

---
+++ src/projects/bms_casa/migrations/22_quote_trans_master.ts
@@ -0,0 +1,66 @@
+import { QueryInterface, DataTypes } from "sequelize"
+module.exports = {
+    up: async (query: QueryInterface) => {
+        await query.createTable("quote_trans_master", {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            constructionCompanyId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "construction_company_master",
+                    key: "id",
+                },
+            },
+            subOpportunityId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "sub_opportunity_master",
+                    key: "id",
+                },
+            },
+            ownerId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "company_master",
+                    key: "id",
+                },
+            },
+            note: {
+                type: DataTypes.STRING(200),
+                allowNull: true,
+            },
+            bid: {
+                type: DataTypes.BOOLEAN,
+                allowNull: true,
+            }, 
+            awarded: {
+                type: DataTypes.BOOLEAN,
+                allowNull: true,
+            },
+            trRequired: {
+                type: DataTypes.BOOLEAN,
+                allowNull: true,
+            },
+            dueDate: {
+                type: DataTypes.DATE,
+                allowNull: true,
+            },
+            createdOn: {
+                type: DataTypes.DATE,
+            },
+            increaseDate: {
+                type: DataTypes.DATE,
+                allowNull: true,
+            },
+            modifiedOn: {
+                type: DataTypes.DATE,
+            },
+        })
+    },
+    down: async (query: QueryInterface) => {
+        await query.dropTable("quote_trans_master")
+    },
+}
---
+++ src/projects/bms_casa/migrations/23_quote_product_master.ts
@@ -0,0 +1,53 @@
+import { QueryInterface, DataTypes } from "sequelize"
+module.exports = {
+    up: async (query: QueryInterface) => {
+        await query.createTable("quote_product_master", {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            transactionId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "quote_trans_master",
+                    key: "id",
+                },
+                allowNull: false,
+            },
+            mdStrength: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },
+            proportions: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },
+            unit: {
+                type: DataTypes.STRING(25),
+                allowNull: false,
+            },
+            estYards: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },
+            conMix: {
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+            },
+            pumpMix: {
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+            },
+            createdOn: {
+                type: DataTypes.DATE,
+            },
+            modifiedOn: {
+                type: DataTypes.DATE,
+            },
+        })
+    },
+    down: async (query: QueryInterface) => {
+        await query.dropTable("quote_product_master")
+    },
+}
